12b04634570759ece938e3e4cf638f3dbccd01b7
Jason <jason27chan@gmail.com>
2020-10-21 15:23:58 -0400

    add vote endpoint

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/health', (req, res) => {
     res.send('App running!');
 });
 
+app.post('/vote', (req, res) => {
+    res.send('Vote Submitted');
+})
+
 app.listen(port, () => {
     console.log(`example app listening at http://localhost:${port}`);
 })
