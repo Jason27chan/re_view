123f7efaaaee1c2e28263e7717bfd4bfd5b0c3c6
Jason <jason27chan@gmail.com>
2020-10-22 17:22:17 -0400

    add passport to index.js

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const dbclass = require('./dbhelper');
+const keys = require('./config/keys');
 
 const dbhelper = new dbclass();
 const app = express();
 const port = 3000;
 
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientId,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (accessToken) => {
+        console.log(accessToken)
+    })
+);
 app.use(express.urlencoded({extended: true}));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
