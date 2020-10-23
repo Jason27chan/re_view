7004edcc47fc7feed5bc79ab9e8375d4d99704c2
Jason <jason27chan@gmail.com>
2020-10-23 17:23:13 -0400

    add passport stuff to index.js

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 
 const dbhelper = require('./dbhelper');
-const keys = require('./config/keys');
+const keys = require('../config/keys');
 
 const app = express();
 const port = 5000;
@@ -14,11 +15,22 @@ passport.use(
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
-        console.log('access token', accessToken);
-        console.log('refresh token', refreshToken);
+        // console.log('access token', accessToken);
+        // console.log('refresh token', refreshToken);
         console.log('profile', profile);
     })
 );
+
+passport.use(
+    new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        session: 'false'
+    }, (username, password, done) => {
+        console.log('username', username);
+        console.log('password', password);
+    })
+)
 
 app.use(express.urlencoded({extended: true}));
 app.use(function(req, res, next) {
@@ -40,7 +52,15 @@ app.get('/auth/google', passport.authenticate('google', {
     })
 );
 
-app.get('/auth/google/callback', passport.authenticate('google'));
+app.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/vote_submitted'}), (req, res) => {
+    res.redirect('/');
+});
+
+app.get('/auth/local', passport.authenticate('local'));
+
+app.get('/auth/local/callback', passport.authenticate('local'), (req, res) => {
+    res.redirect('/');
+})
 
 app.listen(port, () => {
     console.log(`example app listening at http://localhost:${port}`);
