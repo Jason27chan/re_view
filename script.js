5d7b52c3af3dc109c14d045b55747e0355e3c938
Jason <jason27chan@gmail.com>
2020-10-23 10:33:55 -0400

    add loading gif functionality

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 $(function() {
     $("button").click(function() {
         console.log($(this).attr('value'));
+        $(".loading-gif").show();
         $.post("http://localhost:3000/vote", {choice: $(this).attr('value')}, function() {
             console.log('done');
+            $(".loading-gif").hide();
+            
         });
     });
 });
